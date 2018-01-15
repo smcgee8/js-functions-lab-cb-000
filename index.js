28299a1068dbf0c1138285b0bd2450dd030f4351
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-15 14:58:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  `It's ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
